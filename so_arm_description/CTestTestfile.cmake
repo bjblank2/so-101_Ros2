1b88316e057234ce0123ebe850a743d0eda151e7
bjblank2 <bjblank2@illinois.edu>
2025-06-21 21:00:59 -0500

    added description

---
+++ so_arm_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rlab/SO_ROS2/SO-ARM101_MoveIt_IsaacSim/src/so_arm_description
+# Build directory: /home/rlab/SO_ROS2/build/so_arm_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
